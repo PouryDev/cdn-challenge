5fad35f91342c10735a8554852a4ab4459d6b526
Pouriya Khazaei <pouriya.khazaei@arvancloud.com>
2023-12-25 22:15:55 +0330

    add(db): gift code model and migration

---
+++ database/migrations/2023_12_25_184336_create_gift_codes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('gift_codes', function (Blueprint $table) {
+            $table->id();
+            $table->string('code');
+            $table->integer('max_usage')->nullable();
+            $table->integer('used_count')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('gift_codes');
+    }
+};
