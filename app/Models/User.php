9b0fc23a598ea06ecf9168e3ece74163d13fb53b
Pouriya Khazaei <pouriya.khazaei@arvancloud.com>
2023-12-25 22:28:55 +0330

    add(model): transaction relationship in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -43,8 +44,23 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
+    /**
+     * This user's wallet
+     *
+     * @return HasOne
+     */
     public function wallet(): HasOne
     {
         return $this->hasOne(Wallet::class);
+    }
+
+    /**
+     * This user's transactions collection
+     *
+     * @return HasMany
+     */
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
     }
 }
