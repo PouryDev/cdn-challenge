d09e7e6cfeb7740613e37c75613161aa001daa46
Pouriya Khazaei <pouriya.khazaei@arvancloud.com>
2023-12-25 22:28:41 +0330

    add(model): transaction relationship in gift code model

--- app/Models/GiftCode.php
+++ app/Models/GiftCode.php
@@ -3,9 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class GiftCode extends Model
 {
     protected $guarded = [];
     public $incrementing = false;
+
+
+    /**
+     * All transactions created by this gift code
+     *
+     * @return HasMany
+     */
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
