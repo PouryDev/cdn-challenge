fca4ded8a777e9b2a3dd6de0c5213ad8c49364ad
Pouriya Khazaei <pouriya.khazaei@arvancloud.com>
2023-12-25 22:19:20 +0330

    add(migration): user_id field to wallets table

--- database/migrations/2023_12_25_184149_create_wallets_table.php
+++ database/migrations/2023_12_25_184149_create_wallets_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('wallets', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->decimal('balance', 12)->default(0);
+            $table->foreignId('user_id');
             $table->timestamps();
         });
     }
