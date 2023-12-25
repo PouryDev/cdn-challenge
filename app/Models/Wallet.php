886af200dc20f67d042d4a198ef3aa49fa31fcd0
Pouriya Khazaei <pouriya.khazaei@arvancloud.com>
2023-12-25 22:19:39 +0330

    add(model): user relation in wallet model

--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -3,9 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Wallet extends Model
 {
     protected $guarded = [];
     public $incrementing = false;
+
+    /**
+     * The user related to the wallet
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
