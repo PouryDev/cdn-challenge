c1c314a4220de952119b9ed0198fd09f4eced0a3
Pouriya Khazaei <pouriya.khazaei@arvancloud.com>
2024-01-06 02:00:03 +0330

    change: drop unnecessary fields and add phone_number field

---
+++ database/migrations/2024_01_02_170728_customize_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('email');
+            $table->dropColumn('name');
+            $table->dropColumn('password');
+            $table->dropColumn('email_verified_at');
+            $table->dropRememberToken();
+            $table->string('phone_number', 20);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('email')->unique();
+            $table->string('name');
+            $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken();
+            $table->dropColumn('phone_number');
+        });
+    }
+};
