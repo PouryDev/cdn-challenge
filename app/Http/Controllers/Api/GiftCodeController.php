fb755870d0b001bacd029d3a6c792f4db8ba546c
Pouriya Khazaei <pouriya.khazaei@arvancloud.com>
2024-01-06 05:46:11 +0330

    add: gift code controller

---
+++ app/Http/Controllers/Api/GiftCodeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\GiftCodeRequest;
+use App\Jobs\UseGiftCodeJob;
+use App\Models\GiftCode;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+
+class GiftCodeController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(GiftCodeRequest $request): JsonResponse
+    {
+        $data = $request->validated();
+
+        $params = [
+            'phone_number' => $data['phone_number'],
+            'email' => $data['email'],
+        ];
+        $user = User::updateOrCreate($params, $params);
+        if (empty($user)) {
+            return response()->json(
+                ['message' => 'something went wrong please try again'],
+                500
+            );
+        }
+
+        $code = GiftCode::where('code', $data['code'])->first();
+
+        dispatch(new UseGiftCodeJob($code, $user));
+        return response()->json(['message' => 'You can see the result in your mail']);
+    }
+}
